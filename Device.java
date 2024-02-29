7f3aedddeef0d087b263376da07ed2bac81f32db
unknown <marcellopalucci@gmail.com>
2024-02-29 18:17:00 -0500

    added constructor to Devices.java

--- Device.java
+++ Device.java
@@ -5,5 +5,11 @@ public class Device {
     private int cpuCapacity;
     private int cpuRemaining;
     private Task [] tasks;
+    public Device (int serialNumber, int cpuCapacity, int length){
+        this.serialNumber = serialNumber;
+        this.cpuCapacity = cpuCapacity;
+        tasks = tasks[length];
+
+    }
 
 }
